6da90fcaf3d79c2d5bb8fb544b6c07381338e66e
dennis870408 <zhangdenis668@gmail.com>
2025-10-26 22:13:35 +0800

    add basic arithmetic operations

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Calculator");
+Console.WriteLine("輸入第一個數字:");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("輸入第二個數字:");
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("選擇運算方式:");
+char op;
+op = Convert.ToChar(Console.ReadLine());
+
+int result = 0;
+
+switch (op)
+{
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        if (b != 0)
+        {
+            result = a / b;
+        }
+        else
+        {
+            Console.WriteLine("錯誤: 除數不能為零");
+            return;
+        }
+        break;
+    default:
+        Console.WriteLine("錯誤: 無效的運算方式");
+        return;
+}
+
+Console.WriteLine("結果是: " + result);
